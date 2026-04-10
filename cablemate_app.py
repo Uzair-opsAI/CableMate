d1814a86903b6347606d6e414cbc3d1efa8285f8
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-04-10 09:59:01 +0530

    Update cablemate_app.py

--- cablemate_app.py
+++ cablemate_app.py
@@ -188,7 +188,7 @@ col1, col2 = st.columns(2)
 with col1:
     client_name = st.text_input("Client Name", "ABC Pvt Ltd")
     feeder_from = st.selectbox("From Equipment", ["Switchgear", "Transformer", "Generator"])
-    from_tag    = st.write_input("From Equipment Tag", placeholder="e.g. TR-01 / SWGR-A1")
+    from_tag    = st.text_input("From Equipment Tag", placeholder="e.g. TR-01 / SWGR-A1")
     voltage     = st.selectbox("System Voltage (kV)", [3.3, 6.6, 11, 25, 33, 66, 132])
 with col2:
     project_name = st.write_input("Project Name", "Electrical Distribution System")
